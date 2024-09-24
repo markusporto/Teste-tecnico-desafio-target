e0afaa708d8b4bebfa14bc094b243212876e3e8d
markusporto <markusvinicius050@gmail.com>
2024-09-23 21:59:09 -0300

    Resolucao do desafio 03.

---
+++ desafios/src/DesafioTres.java
@@ -0,0 +1,14 @@
+public class DesafioTres {
+    public static void main(String[] args) {
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        for (int i = 1; i < indice; i++) {
+            k = k + i;
+            soma = soma + i;
+            System.out.println(soma);
+
+        }
+    }
+}
